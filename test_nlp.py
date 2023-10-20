aa88a727f2cd980f93631138c829526d41b703ed
sasasiah <88854235+sasasiah@users.noreply.github.com>
2023-10-20 14:35:41 +0700

    test_nlp.py

--- test_nlp.py
+++ test_nlp.py
@@ -5,6 +5,7 @@ from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from nltk.stem import WordNetLemmatizer
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+import plotly.express as px
 
 # Download the necessary NLTK data
 nltk.download('punkt')
@@ -40,10 +41,48 @@ def analyze_sentiment(text):
     return sentiment_score
 
 # Create a Streamlit app
-st.title("NLP App")
+st.title("NLP Dashboard")
 
-text = st.text_input("Enter your text here:")
+text = st.text_area("Enter your text here:")
 
-if text:
+if st.button("Analyze"):
+    # Word Segmentation
+    st.subheader("Word Segmentation:")
+    tokens = word_tokenize(text)
+    st.write(tokens)
+
+    # Sentiment Analysis
     sentiment_score = analyze_sentiment(text)
+    st.subheader("Sentiment Analysis:")
+    st.write(f"Sentiment Score: {sentiment_score['compound']}")
+
+    # Visualization
+    data = pd.DataFrame({'Sentiment': ['Positive', 'Negative', 'Neutral'], 'Count': [sentiment_score['pos'], sentiment_score['neg'], sentiment_score['neu']]})
+    fig = px.bar(data, x='Sentiment', y='Count', color='Sentiment')
+    st.plotly_chart(fig)
+
+# Custom CSS styles
+st.markdown(
+    """
+    <style>
+    .stTextInput {background-color: #F4F4F4;}
+    .stButton {background-color: #1E90FF; color: white;}
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
+# Custom background image
+st.markdown(
+    """
+    <style>
+    body {
+    background-image: url('https://example.com/background-image.jpg');
+    background-size: cover;
+    }
+    </style>
+    """,
+    unsafe_allow_html=True
+)
+
     st.write(f"Sentiment Score: {sentiment_score}")
