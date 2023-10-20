b309095669be6c189f38a8f972b5f900f862ca0c
sasasiah <88854235+sasasiah@users.noreply.github.com>
2023-10-20 14:26:50 +0700

    test_nlp.py

--- test_nlp.py
+++ test_nlp.py
@@ -1,57 +1,49 @@
 import streamlit as st
-import spacy
+import pandas as pd
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import WordNetLemmatizer
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-# Check if the spaCy model is already installed, and if not, download it.
-try:
-    nlp = spacy.load("en_core_web_sm")
-except OSError:
-    st.warning("Downloading spaCy model. This may take a while.")
-    spacy.cli.download("en_core_web_sm")
-    nlp = spacy.load("en_core_web_sm")
+# Download the necessary NLTK data
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
 
-# Set a title and a subtitle
-st.title("NLP Text Analysis")
-st.subheader("Analyze text with spaCy")
+# Initialize the lemmatizer and sentiment analyzer
+lemmatizer = WordNetLemmatizer()
+sentiment_analyzer = SentimentIntensityAnalyzer()
 
-# Create a textarea for user input
-text = st.text_area("Enter text for analysis", "")
+# Define the preprocessing function
+def preprocess_text(text):
+    # Tokenize text
+    tokens = word_tokenize(text)
 
-# Analyze the text using spaCy
+    # Remove stopwords and punctuation
+    tokens = [token.lower() for token in tokens if token.isalnum()]
+    tokens = [token for token in tokens if token not in stopwords.words('english')]
+
+    # Lemmatize tokens
+    tokens = [lemmatizer.lemmatize(token) for token in tokens]
+
+    return tokens
+
+# Define the sentiment analysis function
+def analyze_sentiment(text):
+    # Preprocess text
+    tokens = preprocess_text(text)
+
+    # Analyze sentiment
+    sentiment_score = sentiment_analyzer.polarity_scores(text)
+
+    return sentiment_score
+
+# Create a Streamlit app
+st.title("NLP App")
+
+text = st.text_input("Enter your text here:")
+
 if text:
-    doc = nlp(text)
-
-    # Display the tokenized text
-    st.subheader("Tokenized Text")
-    for token in doc:
-        st.write(token.text)
-
-    # Display named entities
-    st.subheader("Named Entities")
-    for ent in doc.ents:
-        st.write(f"{ent.text} ({ent.label_})")
-
-    # Display parts of speech
-    st.subheader("Parts of Speech")
-    for token in doc:
-        st.write(f"{token.text} - {token.pos_}")
-
-    # Display dependencies
-    st.subheader("Syntactic Dependencies")
-    for token in doc:
-        st.write(f"{token.text} - {token.dep_}")
-
-    # Sentiment Analysis using TextBlob
-    sentiment = TextBlob(text)
-    sentiment_score = sentiment.sentiment.polarity
-    st.subheader("Sentiment Analysis")
-
-    # Display the sentiment score
+    sentiment_score = analyze_sentiment(text)
     st.write(f"Sentiment Score: {sentiment_score}")
-
-    # Categorize the sentiment
-    if sentiment_score > 0:
-        st.write("Sentiment: Positive")
-    elif sentiment_score < 0:
-        st.write("Sentiment: Negative")
-    else:
-        st.write("Sentiment: Neutral")
