36aa3179d542cc123e6dc873d3fee90d1d3ee92c
sasasiah <88854235+sasasiah@users.noreply.github.com>
2023-10-20 10:59:39 +0700

    test_g.py

--- test_g.py
+++ test_g.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 #import plotly.express as px
-from transformers import pipeline
+#from transformers import pipeline
 # Set a title for your Streamlit app
 st.title("Animated 2D Line Chart from CSV")
 
